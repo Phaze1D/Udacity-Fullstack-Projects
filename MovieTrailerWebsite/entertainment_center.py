a37e783f6b453650325fdfa5046ce644752028d3
DavidMBP <steadypathapp@gmail.com>
2017-02-14 21:49:37 -0600

    Added comments

--- MovieTrailerWebsite/entertainment_center.py
+++ MovieTrailerWebsite/entertainment_center.py
@@ -14,12 +14,13 @@ def movies():
     """
 
     rarray = []
-    with open('movies.json') as data_file:
-        data = json.load(data_file)
-        for movie in data['movies']:
+    with open('movies.json') as data_file: # opens movies.json file
+        data = json.load(data_file) # parse json data
+        for movie in data['movies']: # Loop through json array of movies
             rarray.append( Movie(movie['title'], movie['poster_image_url'], movie['trailer_youtube_url']) )
 
     return rarray
 
 
+# Opens the static html page on the default browser
 fresh_tomatoes.open_movies_page( movies() )
