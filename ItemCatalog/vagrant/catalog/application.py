3ef1657a0c8eb17f6af3591baf876e571ade5dd4
DavidMBP <steadypathapp@gmail.com>
2017-04-13 22:07:23 -0500

    Created the application main file

--- ItemCatalog/vagrant/catalog/application.py
+++ ItemCatalog/vagrant/catalog/application.py
@@ -1,9 +1,27 @@
 from flask import Flask
-app = Flask(__name__)
+from backend.controllers import catalogs_app, items_app, users_app
+from backend.api_controllers import catalogs_api_app, items_api_app
+from backend.config import create_schema, connect, generate_csrf_token
+from backend.models import Catalog, Item, User
+
+
+app = Flask(__name__, template_folder='frontend/templates', static_folder='frontend')
+app.jinja_env.globals['csrf_token'] = generate_csrf_token
+
+app.register_blueprint(catalogs_app)
+app.register_blueprint(items_app)
+app.register_blueprint(users_app)
+
+app.register_blueprint(catalogs_api_app)
+app.register_blueprint(items_api_app)
+
+create_schema()
 
 @app.route("/")
-def hello():
+def root():
     return "Hello World!"
+
+
 
 if __name__ == "__main__":
     app.run(port=8080, host='0.0.0.0', debug=True)
