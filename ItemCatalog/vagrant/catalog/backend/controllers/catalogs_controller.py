ba48284f25a0f12a957654ce1dd88ec562ebc722
DavidMBP <steadypathapp@gmail.com>
2017-04-14 22:45:02 -0500

    Finished catalogs implementation

--- ItemCatalog/vagrant/catalog/backend/controllers/catalogs_controller.py
+++ ItemCatalog/vagrant/catalog/backend/controllers/catalogs_controller.py
@@ -1,41 +1,57 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request, jsonify, redirect, url_for
+from backend.models import Item
+from backend.models import Catalog
+from backend.helpers import check_csrf, login_required
+
+import logging
 
 catalogs_app = Blueprint('catalogs', __name__)
 
 
 @catalogs_app.route('/catalogs')
 def index():
-    catalogs = []
+    catalogs = Catalog.get_all()
     return render_template('catalogs/index.html', catalogs=catalogs)
 
 
 @catalogs_app.route('/catalog')
 def create():
-    return render_template('catalogs/create.html')
+    return render_template('catalogs/create.html', catalog=None, error='')
 
 
 @catalogs_app.route('/catalog', methods=['POST'])
+@check_csrf
+@login_required
 def new():
-    return 'new catalog'
+    catalog, error = Catalog.create(name=request.form.get('name'))
+    if error:
+        return render_template('catalogs/create.html',
+                                catalog=request.form,
+                                error=error)
+    else:
+        return redirect(url_for('catalogs.index'))
 
 
 @catalogs_app.route('/catalog/<id>/edit')
 def edit(id):
-    catalog={}
+    catalog = Catalog.find_by_id(id)
     return render_template('catalogs/edit.html', catalog=catalog)
 
 
 @catalogs_app.route('/catalog/<id>/edit', methods=['POST'])
+@check_csrf
+@login_required
 def update(id):
-    return 'update catalog'
+    catalog, error = Catalog.edit(id=id, name=request.form.get('name'))
+    if error:
+        return render_template('catalogs/edit.html',
+                                catalog=request.form,
+                                error=error)
+    else:
+        return redirect(url_for('catalogs.index'))
 
 
 @catalogs_app.route('/catalog/<id>')
 def get(id):
-    catalog={}
-    return render_template('catalogs/get.html', catalog=catalog)
-
-
-@catalogs_app.route('/catalog/<id>', methods=['DELETE'])
-def delete(id):
-    return 'detele catalog'
+    catalog = Catalog.find_by_id(id)
+    return render_template('catalogs/get.html', catalog=catalog, items=catalog.items)
--- ItemCatalog/vagrant/catalog/backend/models/catalog.py
+++ ItemCatalog/vagrant/catalog/backend/models/catalog.py
@@ -1,14 +1,54 @@
-from backend.config import Base
+from backend.config import Base, DBSession
 from sqlalchemy import Column, ForeignKey, Integer, String, DateTime
 from sqlalchemy.sql import func
-from sqlalchemy.orm import relationship
-
+from sqlalchemy.orm import relationship, validates
+import logging
 
 class Catalog(Base):
     __tablename__ = 'catalog'
 
     id          = Column(Integer, primary_key=True)
     name        = Column(String(250), unique=True, nullable=False)
-    description = Column(String(250), nullable=False)
     created     = Column(DateTime, default=func.now())
     items       = relationship("Item", back_populates="catalog")
+
+
+    @validates('name')
+    def validates_name(self, key, name):
+        if len(name) < 3:
+            raise Exception("Name must be atleast 3 chars")
+        return name
+
+
+    @classmethod
+    def create(cls, name):
+        catalog = None
+        error = None
+        try:
+            catalog = cls(name=name)
+            DBSession.add(catalog)
+            DBSession.commit()
+        except Exception as e:
+            error = str(e)
+        return catalog, error
+
+    @classmethod
+    def edit(cls, id, name):
+        catalog = None
+        error = None
+        try:
+            catalog = cls.find_by_id(id)
+            catalog.name = name
+            DBSession.commit()
+        except Exception as e:
+            DBSession.rollback()
+            error = str(e)
+        return catalog, error
+
+    @classmethod
+    def get_all(cls):
+        return DBSession.query(cls).all()
+
+    @classmethod
+    def find_by_id(cls, id):
+        return DBSession.query(cls).filter(cls.id == id).first()
