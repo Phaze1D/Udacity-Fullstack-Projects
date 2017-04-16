f0faa2414f377a6e5f1fe44fce851719a13297e4
DavidMBP <steadypathapp@gmail.com>
2017-04-16 17:17:33 -0500

    Added a flash message and redirect

--- ItemCatalog/vagrant/catalog/backend/helpers/sessions.py
+++ ItemCatalog/vagrant/catalog/backend/helpers/sessions.py
@@ -1,7 +1,8 @@
 from functools import wraps
-from flask import g, request, redirect, url_for, session, abort
+from flask import g, request, redirect, url_for, session, abort, flash
 from backend.models import User
 
+import logging
 
 def login(user):
     session['user'] = user.id
@@ -16,6 +17,7 @@ def login_required(f):
     @wraps(f)
     def decorated_function(*args, **kwargs):
         if 'user' not in session:
-            abort(403)
+            flash('Must be logged in')
+            return redirect(url_for('catalogs.index'))
         return f(*args, **kwargs)
     return decorated_function
