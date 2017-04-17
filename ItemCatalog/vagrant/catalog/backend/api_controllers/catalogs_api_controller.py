53c530a2ac5f5da32132ba7e4a43b5da4b1813ab
DavidMBP <steadypathapp@gmail.com>
2017-04-16 19:35:56 -0500

    Added items and catalog api

--- ItemCatalog/vagrant/catalog/backend/api_controllers/catalogs_api_controller.py
+++ ItemCatalog/vagrant/catalog/backend/api_controllers/catalogs_api_controller.py
@@ -1,38 +1,21 @@
-from flask import Blueprint, render_template, abort
+from flask import Blueprint, render_template, abort, request, jsonify, redirect, url_for
+from backend.models import Item
+from backend.models import Catalog
+from backend.helpers import check_csrf, login_required, catalog_exists
+
 
 catalogs_api_app = Blueprint('catalogs_api', __name__)
 
 
 @catalogs_api_app.route('/api/catalogs')
 def index():
-    return 'index catalogs'
+    catalogs = [catalog.to_json() for catalog in Catalog.get_all()]
+    return jsonify(catalogs=catalogs)
 
-
-@catalogs_api_app.route('/api/catalog')
-def create():
-    return 'create catalog'
-
-
-@catalogs_api_app.route('/api/catalog', methods=['POST'])
-def new():
-    return 'new catalog'
-
-
-@catalogs_api_app.route('/api/catalog/<id>/edit')
-def edit(id):
-    return 'edit catalog'
-
-
-@catalogs_api_app.route('/api/catalog/<id>/edit', methods=['POST'])
-def update(id):
-    return 'update catalog'
 
 
 @catalogs_api_app.route('/api/catalog/<id>')
+@catalog_exists
 def get(id):
-    return 'get catalog'
-
-
-@catalogs_api_app.route('/api/catalog/<id>', methods=['DELETE'])
-def delete(id):
-    return 'detele catalog'
+    catalog = Catalog.find_by_id(id)
+    return jsonify(catalog.to_json())
--- ItemCatalog/vagrant/catalog/backend/api_controllers/items_api_controller.py
+++ ItemCatalog/vagrant/catalog/backend/api_controllers/items_api_controller.py
@@ -1,39 +1,18 @@
-from flask import Blueprint, render_template, abort
-
+from flask import Blueprint, render_template, abort, request, redirect, url_for, jsonify
+from backend.models import Item
+from backend.models import Catalog
+from backend.helpers import check_csrf, login_required, current_user, item_exists, item_belongs
 
 items_api_app = Blueprint('items_api', __name__)
 
-
-@items_api_app.route('/api/catalog/<catalog_id>/items')
-def index(catalog_id):
-    return 'index items'
-
-
-@items_api_app.route('/api/catalog/<catalog_id>/item')
-def create(catalog_id):
-    return 'create item'
-
-
-@items_api_app.route('/api/catalog/<catalog_id>/item', methods=['POST'])
-def new(catalog_id):
-    return 'new item'
-
-
-@items_api_app.route('/api/item/<id>/edit')
-def edit(id):
-    return 'edit item'
-
-
-@items_api_app.route('/api/item/<id>/edit', methods=['POST'])
-def update(id):
-    return 'update item'
+@items_api_app.route('/api/items')
+def index():
+    items = [item.to_json() for item in Item.get_all()]
+    return jsonify(items=items)
 
 
 @items_api_app.route('/api/item/<id>')
+@item_exists
 def get(id):
-    return 'get item'
-
-
-@items_api_app.route('/api/item/<id>', methods=['DELETE'])
-def delete(id):
-    return 'detele item'
+    item=Item.find_by_id(id)
+    return jsonify(item.to_json())
