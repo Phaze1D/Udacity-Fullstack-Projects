9b56fbc6350142f10d7207c9929ecc11d4720fa9
DavidMBP <steadypathapp@gmail.com>
2017-04-28 19:03:56 -0500

    Changed to postgresql

--- ItemCatalog/vagrant/catalog/backend/config/database.py
+++ ItemCatalog/vagrant/catalog/backend/config/database.py
@@ -4,7 +4,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session
 
-engine = create_engine('sqlite:///item_catalog', echo=True)
+engine = create_engine("postgresql+psycopg2://catalog:@/catalog")
 Base = declarative_base()
 DBSession = scoped_session(sessionmaker(bind=engine))
 
